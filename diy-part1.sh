5fed83bc830b39170fb9ea2f1c7dffd61e412bf9
zyz <31105177+zyz1130083243@users.noreply.github.com>
2024-08-29 22:03:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git luci-theme-argon https://github.com/jerrykuku/luci-theme-argon.git' >> feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
