8dfd4f642ff7e61d71df8985192ac3c4f5d166a9
zyz <31105177+zyz1130083243@users.noreply.github.com>
2024-08-30 04:54:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,9 +23,9 @@ sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generat
 sed -i 's/set wireless.${name}.disabled=1/set wireless.${name}.disabled=0/' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # 修改kernel内核校验值
-echo 03ba5b5fee47f2232a088e3cd9832aec >> .vermagic
-cat .vermagic
-sed -i '/vermagic/s/.*/  cp $(TOPDIR)\/vermagic $(LINUX_DIR)\/.vermagic/' include/kernel-defaults.mk
+# echo 03ba5b5fee47f2232a088e3cd9832aec >> .vermagic
+# cat .vermagic
+# sed -i '/vermagic/s/.*/  cp $(TOPDIR)\/vermagic $(LINUX_DIR)\/.vermagic/' include/kernel-defaults.mk
 # sed -i '/md5/s/.*/  STAMP_BUILT:=$(STAMP_BUILT)_$(shell cat $(LINUX_DIR)\/.vermagic)/' package/kernel/linux/Makefile
 
 # 修改 LUCI 源
