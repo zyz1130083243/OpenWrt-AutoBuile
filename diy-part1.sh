a51b8350ca1dd381dc9b7f597c6841e82dea0d5e
zyz <31105177+zyz1130083243@users.noreply.github.com>
2024-08-31 16:10:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,3 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/feeds/luci/themes/luci-theme-argon
